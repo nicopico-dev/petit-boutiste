daf4f135b5037495408d3a07b30087488b40f4fb
Nicolas Picon <nicopico.dev@gmail.com>
2025-08-07 08:25:20 +0200

    feat: set blue as primary theme color

---
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/PetitBoutisteTheme.kt
@@ -0,0 +1,20 @@
+package fr.nicopico.petitboutiste.ui
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val PetitBoutisteColorScheme = lightColorScheme(
+    primary = Color(0xFF1F3D91),
+    primaryContainer = Color(0xFFDEE4F8)
+)
+
+@Composable
+fun PetitBoutisteTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = PetitBoutisteColorScheme,
+        content = content
+    )
+}
+
--- composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/infra/preview/WrapForPreview.kt
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/infra/preview/WrapForPreview.kt
@@ -1,13 +1,13 @@
 package fr.nicopico.petitboutiste.ui.infra.preview
 
-import androidx.compose.material3.MaterialTheme
+import fr.nicopico.petitboutiste.ui.PetitBoutisteTheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun WrapForPreview(
     content: @Composable () -> Unit
 ) {
-    MaterialTheme {
+    PetitBoutisteTheme {
         content()
     }
 }
