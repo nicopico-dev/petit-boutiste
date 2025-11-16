9f8cbf780c530d544b2bd359cf53ab518eb17f94
Nicolas Picon <nicopico.dev@gmail.com>
2025-11-16 15:56:00 +0100

    chore: prepare infra for calculator

--- composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/components/definition/ByteGroupDefinitionForm.kt
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/components/definition/ByteGroupDefinitionForm.kt
@@ -24,6 +24,7 @@ import fr.nicopico.petitboutiste.models.ByteGroupDefinition
 import fr.nicopico.petitboutiste.ui.components.foundation.PBLabel
 import fr.nicopico.petitboutiste.ui.components.foundation.PBLabelOrientation.Horizontal
 import fr.nicopico.petitboutiste.ui.components.foundation.PBTextField
+import fr.nicopico.petitboutiste.utils.compute
 import fr.nicopico.petitboutiste.utils.preview.WrapForPreviewDesktop
 import org.jetbrains.jewel.ui.component.DefaultButton
 import org.jetbrains.jewel.ui.component.Text
@@ -50,7 +51,7 @@ fun ByteGroupDefinitionForm(
     val startIndexError by remember(definition) {
         derivedStateOf {
             if (startIndexInput.isNotEmpty()) {
-                val startIndex = startIndexInput.toIntOrNull()
+                val startIndex = compute(startIndexInput)
                 when {
                     startIndex == null -> "Must be a number"
                     startIndex < 0 -> "Must be a positive number"
@@ -62,8 +63,8 @@ fun ByteGroupDefinitionForm(
     val endIndexError by remember(definition) {
         derivedStateOf {
             if (endIndexInput.isNotEmpty()) {
-                val startIndex = startIndexInput.toIntOrNull()
-                val endIndex = endIndexInput.toIntOrNull()
+                val startIndex = compute(startIndexInput)
+                val endIndex = compute(endIndexInput)
                 when {
                     endIndex == null -> "Must be a number"
                     endIndex < (startIndex ?: 0) -> "Must be greater than or equal to Start"
@@ -82,7 +83,7 @@ fun ByteGroupDefinitionForm(
 
     val saveDefinition: () -> Unit = {
         val definitionToSave = definition.copy(
-            indexes = startIndexInput.toInt()..endIndexInput.toInt(),
+            indexes = compute(startIndexInput)!!..compute(endIndexInput)!!,
             name = name.ifBlank { null },
         )
         onDefinitionSaved(definitionToSave)
