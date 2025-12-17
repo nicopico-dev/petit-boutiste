58d9f8b7f608cb8181b7aa8c94522915c43b7df8
Nicolas Picon <nicopico.dev@gmail.com>
2025-12-17 23:30:58 +0100

    feat: save template with paths relative to the template file

--- composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/repository/TemplateManagerImpl.kt
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/repository/TemplateManagerImpl.kt
@@ -7,13 +7,17 @@
 package fr.nicopico.petitboutiste.repository
 
 import fr.nicopico.petitboutiste.models.persistence.Template
+import fr.nicopico.petitboutiste.models.representation.Representation
+import fr.nicopico.petitboutiste.models.representation.arguments.ArgumentType
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import kotlinx.serialization.json.encodeToStream
 import java.io.File
+import java.nio.file.Path
 import kotlin.io.path.createDirectories
 import kotlin.io.path.createTempFile
+import kotlin.io.path.relativeTo
 
 @OptIn(ExperimentalSerializationApi::class)
 class TemplateManagerImpl(
@@ -21,8 +25,11 @@ class TemplateManagerImpl(
 ) : TemplateManager {
 
     override suspend fun loadTemplate(templateFile: File): Template {
-        return templateFile.inputStream().buffered().use { stream ->
+        val template = templateFile.inputStream().buffered().use { stream ->
             json.decodeFromStream<Template>(stream)
+        }
+        return template.updateFileArgumentPaths { relativePath ->
+            File(templateFile.parentFile, relativePath).absolutePath
         }
     }
 
@@ -35,14 +42,49 @@ class TemplateManagerImpl(
         directory.createDirectories()
 
         val tempFile = createTempFile(directory).toFile()
+        val templateWithRelativePaths = template.updateFileArgumentPaths { absolutePath ->
+            File(absolutePath).relativePath(directory)
+        }
 
         try {
             tempFile.outputStream().buffered().use { stream ->
-                json.encodeToStream<Template>(template, stream)
+                json.encodeToStream<Template>(templateWithRelativePaths, stream)
             }
             tempFile.copyTo(templateFile, overwrite = overwrite)
         } finally {
             tempFile.delete()
         }
     }
+}
+
+private fun Template.updateFileArgumentPaths(
+    transform: (String) -> String,
+): Template {
+    return copy(
+        definitions = definitions.map { definition ->
+            definition.copy(
+                representation = definition.representation.updateFileArgumentPaths(transform)
+            )
+        }
+    )
+}
+
+private fun Representation.updateFileArgumentPaths(
+    transform: (String) -> String,
+): Representation {
+    val fileKeys = dataRenderer.arguments
+        .filter { it.type is ArgumentType.FileType }
+        .map { it.key }
+    val updatedArgValues = argumentValues
+        .mapValues { (key, value) ->
+            if (key in fileKeys) {
+                transform(value)
+            } else value
+        }
+
+    return copy(argumentValues = updatedArgValues)
+}
+
+private fun File.relativePath(baseDir: Path): String {
+    return absoluteFile.toPath().relativeTo(baseDir).toString()
 }
