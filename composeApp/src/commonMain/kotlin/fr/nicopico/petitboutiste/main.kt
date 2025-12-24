afdb15a715cc2c5f7f27116617c2140182063f26
Nicolas Picon <nicopico.dev@gmail.com>
2025-12-24 12:27:42 +0100

    chore: introduce ViewModel (fixed with Junie)

--- composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/main.kt
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/main.kt
@@ -6,7 +6,18 @@
 
 package fr.nicopico.petitboutiste
 
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.application
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import fr.nicopico.petitboutiste.ui.theme.system.followSystemTheme
 import io.github.vinceglb.filekit.FileKit
 
@@ -20,8 +31,36 @@ fun main() {
     followSystemTheme()
 
     application {
-        PetitBoutiste(
-            onAppClose = { exitApplication() },
-        )
+        val viewModelStoreOwner = remember {
+            object : ViewModelStoreOwner {
+                override val viewModelStore: ViewModelStore = ViewModelStore()
+            }
+        }
+
+        val lifecycleOwner = remember {
+            object : LifecycleOwner {
+                override val lifecycle: Lifecycle = LifecycleRegistry(this)
+            }
+        }
+
+        LaunchedEffect(lifecycleOwner) {
+            (lifecycleOwner.lifecycle as LifecycleRegistry).handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        }
+
+        DisposableEffect(viewModelStoreOwner, lifecycleOwner) {
+            onDispose {
+                (lifecycleOwner.lifecycle as LifecycleRegistry).handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                viewModelStoreOwner.viewModelStore.clear()
+            }
+        }
+
+        CompositionLocalProvider(
+            LocalViewModelStoreOwner provides viewModelStoreOwner,
+            LocalLifecycleOwner provides lifecycleOwner,
+        ) {
+            PetitBoutiste(
+                onAppClose = { exitApplication() },
+            )
+        }
     }
 }
