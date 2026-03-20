af70982490269fa8c506bf785318fa0306112ac1
Nicolas Picon <nicopico.dev@gmail.com>
2026-03-20 23:26:09 +0100

    fix: timeout exception spam (#74)

--- composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/components/representation/ArgumentInput.kt
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/ui/components/representation/ArgumentInput.kt
@@ -32,6 +32,7 @@ import fr.nicopico.petitboutiste.utils.compose.optionalSlot
 import fr.nicopico.petitboutiste.utils.logError
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
@@ -49,14 +50,16 @@ fun ArgumentInput(
     modifier: Modifier = Modifier,
     onError: (String) -> Unit = {},
 ) {
-    val value: ArgValue? = remember(argument, userValue) {
-        userValue ?: argument.defaultValue
-    }
-    val completeArgumentsFlow = remember(argument, userValue) {
+    // Build a persistent `completeArgumentsFlow`
+    val completeArgumentsFlow = remember {
         MutableSharedFlow<ArgumentValues>(replay = 1)
     }
     LaunchedEffect(completeArgumentsFlow, completeArguments) {
         completeArgumentsFlow.emit(completeArguments)
+    }
+
+    val value: ArgValue? = remember(argument, userValue) {
+        userValue ?: argument.defaultValue
     }
 
     Column(modifier = modifier) {
@@ -91,8 +94,10 @@ fun ArgumentInput(
                     with(argument.type) {
                         val choices by getChoices(completeArgumentsFlow)
                             .flowOn(Dispatchers.Default)
-                            .timeout(2.seconds)
+                            .timeout(4.seconds)
                             .catch { error ->
+                                if (error is TimeoutCancellationException) throw error
+
                                 logError("Error parsing choices for ${argument.key}", error)
                                 onError("Error parsing choices for ${argument.label}")
                                 emit(emptyList())
