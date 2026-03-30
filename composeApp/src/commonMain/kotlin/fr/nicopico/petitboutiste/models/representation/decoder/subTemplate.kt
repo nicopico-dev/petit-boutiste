ed5887c83c3a7ae8380b846b69d25e70be0ac4bd
Nicolas Picon <nicopico.dev@gmail.com>
2026-03-30 21:03:55 +0200

    fix: handle rendering error correctly

--- composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/models/representation/decoder/subTemplate.kt
+++ composeApp/src/commonMain/kotlin/fr/nicopico/petitboutiste/models/representation/decoder/subTemplate.kt
@@ -62,7 +62,7 @@ suspend fun DataRenderer.decodeSubTemplate(byteArray: ByteArray, argumentValues:
                 is RenderResult.Structured -> renderResult.data
                 is RenderResult.Error -> {
                     // Early return for any sub-template error
-                    return "SUB-TEMPLATE ERROR in $groupName: ${renderResult.message}"
+                    error("SUB-TEMPLATE ERROR in $groupName: ${renderResult.message}")
                 }
             }
 
